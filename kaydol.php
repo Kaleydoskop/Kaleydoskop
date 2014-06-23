e5b870988a4e6b3946b48538a12d0147d61aa787
Tan <tanaytekin@gmail.com>
2014-06-23 19:35:23 +0300

    update
    .....

--- kaydol.php
+++ kaydol.php
@@ -9,16 +9,34 @@ class ajaxValidate {
         $surname = @$_POST["surname"];
         $email = @$_POST["email"];
         $password = @$_POST["password"];
-$return = array();
-$return['msg'] = '<div class="alert alert-success fade in">
+
+		$return = array();
+		$email_kontrol = mysql_query("SELECT * FROM accounts WHERE email='{$email}'"); 
+		$email_kontrol2 = mysql_num_rows($email_kontrol); 
+		if($email_kontrol2>0)
+		{$return['msg'] = '<div class="alert alert-danger fade in">
        <span class="close" data-dismiss="alert">&times;</span>
-       	Üye kaydın tamamlandı. Sağ taraftan giriş yapabilirsin! </div>'.$name;
+       	Bu e-mail kullanılıyor ! </div>';
+
+       }else
+       {
+       		$password1 = hash("sha512",$password);	
+       				mysql_query("INSERT INTO accounts(id,name,surname,email,password) values (null, '$name','$surname','$email','$password1')");
+	
+			$return['msg'] = '<div class="alert alert-success fade in">
+       <span class="close" data-dismiss="alert">&times;</span>
+       	Üye kaydın tamamlandı. Sağ taraftan giriş yapabilirsin! </div>';
+       }
+			
+			return json_encode($return);
+		
+			
+			
 
 
 
 
 
-return json_encode($return);
 }
 }
 
